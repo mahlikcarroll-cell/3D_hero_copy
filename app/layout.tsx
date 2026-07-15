b766ab2494f40b589e600aef3992dd03ca2ba424
mahlikcarroll-cell <mahlikcarroll@gmail.com>
2026-07-15 11:24:04 -0500

    added new UI modules for testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 
 import "../styles/main.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 const montserrat = localFont({
   src: [
@@ -31,7 +35,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", inter.variable)}>
       <body className={montserrat.variable}>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 "use client"
 import { useEffect } from "react";
-
+import { Button } from "@/components/ui/button";
 import initPlanet3D from "@/components/3D/planet"
+import { MagicCard } from "@/components/ui/magic-card"
+import  MacbookScrollDemo from "@/components/macbook-scroll-demo"
+import { MacbookScroll } from "@/components/ui/macbook-scroll";
+
 
 export default function Home() {
 
@@ -21,16 +25,21 @@ export default function Home() {
     <div className="page">
       <section className="hero_main">
         <div className="content">
-          <h1>Welcome To Mach10 Creative.</h1>
-
+          <h1 className="hero_main text-red-500">
+  Welcome To Mach10 Creative.
+</h1>
           <p>
             We build one-of-one systems engineered to keep your business moving.
           </p>
-
           <button className="cta_btn">Get started.</button>
+        
         </div>
         <canvas className="planet-3D" />
       </section>
+<section> 
+ <MacbookScrollDemo></MacbookScrollDemo>
+</section>
+          
     </div>
   );
 }
